9f7c86cf176b7b85901c93c7fc5f0fcec105e538
Maxime Leriche <maxleriche.60@gmail.com>
2021-12-23 16:31:51 +0100

    [add] Examen handler

--- src/component/PageExamen.tsx
+++ src/component/PageExamen.tsx
@@ -1,26 +1,73 @@
 import { observer } from "mobx-react-lite";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { RevisionContext } from "..";
+import { Examen } from "../model/Examen";
+import Modal from "./modal";
 
-const Examen = observer(() => {
+const Examens = observer(() => {
   const Revision = useContext(RevisionContext);
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+  const [objectInEdit, setObjectInEdit] = useState<Examen>({} as Examen);
+  const setModalClose = () => {
+    setModalIsOpen(false);
+    Revision.AddExamen(objectInEdit.Name, objectInEdit.Date);
+    setObjectInEdit({} as Examen);
+  };
   return (
-    <div className="flex">
+    <div>
+      <Modal
+        on={modalIsOpen}
+        deactivate={() => {
+          setModalIsOpen(false);
+          setObjectInEdit({} as Examen);
+        }}
+        deactivateText="Annuler"
+        activate={() => setModalClose()}
+        activateText="Créer"
+        title="Création un Examen"
+        Content={() => (
+          <div className="flex flex-col">
+            <label htmlFor="ExamenName">Nom de l'examen :</label>
+            <input
+              name="ExamenName"
+              type="text"
+              value={objectInEdit.Name}
+              onChange={(e) => {
+                objectInEdit.Name = e.currentTarget.value;
+                setObjectInEdit(objectInEdit);
+              }}
+            />
+            <label htmlFor="ExamenDate">Date de l'examen :</label>
+            <input
+              name="ExamenDate"
+              type="date"
+              value={objectInEdit.Date?.toDateString()}
+              onChange={(e) => {
+                if (e.currentTarget.valueAsDate != null)
+                  objectInEdit.Date = e.currentTarget.valueAsDate;
+                setObjectInEdit(objectInEdit);
+              }}
+            />
+          </div>
+        )}
+      />
+      <button onClick={() => setModalIsOpen(true)}>Add Examen</button>
       {Revision.Examens.map(({ id, Name, Date, Fiche }) => (
         <div key={id}>
           <p>
-            {id}&gt;{Name}:{Date}
+            {id}&gt;{Name} : {Date}
           </p>
-          {Fiche.map(({ idDeLaFiche, ImportanceDeLaFiche, Apprentissage }) => (
-            <div key={idDeLaFiche}>
-              <p>ID de la fiche : {idDeLaFiche}</p>
-              <p>Importance : {ImportanceDeLaFiche}</p>
-            </div>
-          ))}
+          {Fiche &&
+            Fiche.map(({ idDeLaFiche, ImportanceDeLaFiche, Apprentissage }) => (
+              <div key={idDeLaFiche}>
+                <p>ID de la fiche : {idDeLaFiche}</p>
+                <p>Importance : {ImportanceDeLaFiche}</p>
+              </div>
+            ))}
         </div>
       ))}
     </div>
   );
 });
 
-export default Examen;
+export default Examens;
